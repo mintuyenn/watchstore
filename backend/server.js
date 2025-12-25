a3c77a7f182cdf1111bd8cf307bf3951a73f2d4b
mintuyenn <caotuyen281005@gmail.com>
2025-12-25 19:25:29 +0700

    Update server to fix bug when deploying

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://watchstoree-production.up.railway.app"],
     credentials: true,
   })
 );
